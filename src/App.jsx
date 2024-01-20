d98c89cf8a4b9b53b4ef51efc883616c84c68164
SabinaCristea <sabinacristea997@gmail.com>
2024-01-20 22:15:54 +0200

    changed import wrong name

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
-import AppLayout from './ui/Applayout';
+import AppLayout from './ui/AppLayout';
 import { action as updateOrderAction } from './features/order/UpdateOrder';
 
 const router = createBrowserRouter([
